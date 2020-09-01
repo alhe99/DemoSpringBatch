a5d64b60c70712951c391e3b1007e4a426a547f8
Luis Alonso Hernández <luisalonsohdezz@gmail.com>
2020-08-31 21:44:43 -0600

    Adding fake enpoint

--- src/main/java/com/spring/batch/demo/app/controller/BaseController.java
+++ src/main/java/com/spring/batch/demo/app/controller/BaseController.java
@@ -1,4 +1,13 @@
 package com.spring.batch.demo.app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BaseController {
+
+    @GetMapping("/home")
+    public String returnHome() {
+        return "Hello World";
+    }
 }
